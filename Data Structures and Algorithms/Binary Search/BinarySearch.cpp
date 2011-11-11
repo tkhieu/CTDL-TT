4647b263672fd3ad514a06aae28f254438bf96eb
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-11-12 00:36:15 +0700

    Hoàn thành Project cơ bản về tạo mãng và Sắp xếp siêu Basic. Đã làm xong Tìm kiếm tuyến tính và tìm kiếm nhị phân.

---
+++ Data Structures and Algorithms/Binary Search/BinarySearch.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void RandomArray(int a[], int &n);
+void PrintArray(int a[], int n);
+void Swap(int &m, int &n);
+void SimpleSort(int a[], int n);
+int BinarySearch(int a[], int n, int x);
+
+void main()
+{
+	int a[100];
+	int n;
+	RandomArray(a,n);
+	PrintArray(a,n);
+	SimpleSort(a,n);
+	PrintArray(a,n);
+	int x;
+	printf("\nNhap x: ");
+	scanf("%d",&x);
+	int j = BinarySearch(a,n,x);
+	printf("Vi tri tim thay: %d",j);
+	getch();
+}
+
+void RandomArray(int a[], int &n)
+{
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	srand(time(NULL));
+	for(int i=0;i<n;i++)
+	{
+		a[i] = rand() % 99;
+	}
+}
+
+void PrintArray(int a[], int n)
+{
+	printf("\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%3d ",a[i]);
+	}
+}
+
+void SimpleSort(int a[], int n)
+{
+	for(int i=0;i<n-1;i++)
+		for(int j=i+1;j<n;j++)
+			if(a[i] > a[j])
+				Swap(a[i],a[j]);
+}
+
+void Swap(int &m, int &n)
+{
+	int temp = m;
+	m = n;
+	n=temp;
+}
+
+
+int BinarySearch(int a[], int n, int x)
+{
+	int left = 0;
+	int right = n-1;
+	int mid = (left + right)/2;
+	do{
+		if(a[mid] == x)
+		return mid;
+	if(a[mid] > x)
+		right = mid -1;
+	else
+		left=mid+1;
+	} while (left <= right);
+	return -1;
+}
---
+++ Data Structures and Algorithms/Linear Search/LinearSearch.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Nguyên mẫu hàm
+void RandomArray(int a[], int &n);
+void PrintArray(int a[], int n);
+int LinearSearch(int a[], int n, int x);
+
+// Hàm main
+void main()
+{
+	int a[100];
+	int n;
+	RandomArray(a,n);
+	PrintArray(a,n);
+	int x;
+	printf("\nNhap x: ");
+	scanf("%d",&x);
+	int j = LinearSearch(a,n,x);
+	printf("Vi tri tim thay: %d",j);
+	getch();
+}
+
+// Khởi tạo một mãng ngẫu nhiên
+void RandomArray(int a[], int &n)
+{
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	// Sử dụng srand để kiến tạo seed
+	srand(time(NULL));
+	for(int i=0;i<n;i++)
+	{
+		a[i] = rand() % 99;
+	}
+}
+
+void PrintArray(int a[], int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("%3d ",a[i]);
+	}
+}
+
+/*
+Hàm LinearSearch
+Đây là thuật toán tìm kiếm đơn giản nhất
+*/
+
+int LinearSearch(int a[], int n, int x)
+{
+	int i = 0;
+	// Điều kiện lặp
+	while(i<=n-1)
+	{
+		if(a[i] == x)
+			return i;
+		i++;
+	}
+	return -1;
+}

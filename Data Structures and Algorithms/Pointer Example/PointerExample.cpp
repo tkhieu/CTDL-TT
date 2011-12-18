c99456b1241e4683bd50714e2f85eb9708815542
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-12-18 16:39:09 +0700

    Hoán vị truyền Tham Trị

--- Data Structures and Algorithms/Pointer Example/PointerExample.cpp
+++ Data Structures and Algorithms/Pointer Example/PointerExample.cpp
@@ -1,10 +1,32 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <conio.h>
+
+
+void HoanVi(int a, int b)
+{
+	int t = a; a = b; b = t;
+}
 
 void main()
 {
-	int p;
-	printf("%p",&p);
+	int a = 10, b = 20;
+	int *pa, *pb;
+
+	pa = &a;
+	pb = &b;
+
+	printf("a = %d \nb = %d",a,b);
+
+	// Lấy giá trị từ biến con trỏ
+	printf("\n%d",*pa);
+	// Lấy địa chỉ ô nhớ từ biến con trỏ
+	printf("\n%p",pa);
+	printf("\n%p",&pa);
+	printf("\n%p",&a);
+
+	printf("\nHoan vi Truyen Tham Tri");
+	HoanVi(a,b);
+	printf("\na:%d b:%d",a,b);
 
 	getch();
 }

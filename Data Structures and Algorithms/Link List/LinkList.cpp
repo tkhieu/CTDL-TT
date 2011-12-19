1fd794fc6606fb23df49437b7d8bcce2c29da7ce
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-12-19 10:59:42 +0700

    FINISH: Hoàn thành GetNode và 2 thao tác chèn vào đầu danh sách

--- Data Structures and Algorithms/Link List/LinkList.cpp
+++ Data Structures and Algorithms/Link List/LinkList.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <conio.h>
 #include <string>
 
@@ -11,7 +11,7 @@ struct Data {
 typedef struct Data DATA;
 
 struct Node {
-	struct Info *pNext;
+	struct Node *pNext;
 	DATA Data;
 };
 typedef struct Node NODE;
@@ -21,3 +21,56 @@ struct LinkList {
 	Node *pTail;
 };
 typedef struct LinkList LIST;
+
+void main(){
+
+}
+
+
+// CÁC TÁC VỤ
+// 01. GetNode: Khởi tạo Node
+NODE *GetNode(DATA d){
+	NODE *p;
+	p = new NODE;
+	if (p == NULL)
+	{
+		printf("\nKhong du bo nho");
+		exit(1);
+	} else
+	{
+		p->Data = d;
+		p->pNext = NULL;
+	}
+	return p;
+}
+
+// 02. Insert: Chèn một dữ liệu vào DSLK
+// 02.1: AddFirst: Chèn vào đầu danh sách
+void AddFirst(LIST &l, NODE *node)
+{
+	if (l.pHead == NULL)
+	{
+		l.pTail = node;
+		l.pHead = node;
+	} else {
+		node->pNext = l.pHead;
+		l.pHead = node;
+	}
+}
+
+NODE *InsertHead(LIST &l, Data d){
+	NODE *node = GetNode(d);
+	if (node == NULL)
+	{
+		return NULL;
+	}
+	if (l.pHead == NULL)
+	{
+		l.pTail = node;
+		l.pHead = node;
+	} else {
+		node->pNext = l.pHead;
+		l.pHead = node;
+	}
+	return node;
+}
